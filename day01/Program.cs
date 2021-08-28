c54776426ba05e4c846003184f3ca47e654a27c5
Jacob Cazacov <jacob.cazacov@gmail.com>
2021-08-28 14:05:43 +0200

    Advent of Code 2015 Days 1 & 2

---
+++ day01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace day01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            string[] allLines = File.ReadAllLines("Input.txt");
+
+            int floor = 0;
+            int enter = 1;
+            bool first = true;
+
+            var instr = allLines[0]; 
+
+            for (int i = 0; i < instr.Length; i++)
+            {
+                if (instr[i] == '(')
+                {
+                    floor = floor + 1;
+                }
+                if (instr[i] == ')')
+                {
+                    floor = floor - 1;
+                }
+
+                if (floor <= -1 && first)
+                {
+                    enter = i + 1;
+                    first = false;
+                }
+            }
+            Console.WriteLine(floor);
+            Console.WriteLine(enter);
+        }
+    }
+}
---
+++ day02/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<List<int>> sizes = new List<List<int>>();
+            string[] allLines = File.ReadAllLines("Input.txt");
+            
+            foreach (string line in allLines)
+            {
+                var lineList = new List<int>();
+                var strs = line.Split(new char[] {'x'});
+                foreach (string dim in strs)
+                {
+                    lineList.Add(Int32.Parse(dim));
+                }                
+                sizes.Add(lineList);
+            }
+
+            int paper = 0;
+            int ribbon = 0;
+
+            for (int num = 0; num < sizes.Count; num++)
+            {
+                List<int> order = sizes[num];
+                int minside;
+
+                int side1 = order[0] * order[1];
+                minside = side1;
+                
+                int side2 = order[1] * order[2];
+                if (side2 < minside)
+                {
+                    minside = side2;
+                }
+                    
+                int side3 = order[0] * order[2];
+                if (side3 < minside)
+                {
+                    minside = side3;
+                }
+
+                int minrib;
+                
+                int rib1 = order[0] * 2 + order[1] * 2;
+                minrib = rib1;
+                
+                int rib2 = order[1] * 2 + order[2] * 2;
+                if (rib2 < minrib)
+                {
+                    minrib = rib2;
+                }
+                    
+                int rib3 = order[0] * 2 + order[2] * 2;
+                if (rib3 < minrib)
+                {
+                    minrib = rib3;
+                }
+
+                int vol = order[0] * order[1] * order[2];
+
+                
+                
+                
+                int size = side1 * 2 + side2 * 2 + side3 * 2 + minside;
+                int riball = minrib + vol;
+                paper = paper + size;
+                ribbon = ribbon + riball;
+            }
+            Console.WriteLine(paper);
+            Console.WriteLine(ribbon);
+        }
+    }
+}
