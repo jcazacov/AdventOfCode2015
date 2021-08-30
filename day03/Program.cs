e5873598fb29b6d85ed185037fe333170c0100ce
Jacob Cazacov <jacob.cazacov@gmail.com>
2021-08-30 11:39:30 +0200

    Day 3 & 5

---
+++ day03/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] allLines = File.ReadAllLines("Input.txt");
+            var instr = allLines[0];
+
+            int x1 = 0;
+            int y1 = 0;
+            int x2 = 0;
+            int y2 = 0;
+            int visit = 1;
+
+            Dictionary<string, int> houses = new Dictionary<string, int>();
+            houses.Add("0;0", 1);
+
+
+            for (int i = 0; i < instr.Length; i++)
+            {
+                if (instr[i] == '^')
+                {
+                    if (i % 2 == 0)
+                    {
+                        y1++;
+                    }
+                    else
+                    {
+                        y2++;
+                    }
+                }
+
+                if (instr[i] == 'v')
+                {
+                    if (i % 2 == 0)
+                    {
+                        y1--;
+                    }
+                    else
+                    {
+                        y2--;
+                    }
+                }
+
+                if (instr[i] == '<')
+                {
+                    if (i % 2 == 0)
+                    {
+                        x1--;
+                    }
+                    else
+                    {
+                        x2--;
+                    }
+                }
+
+                if (instr[i] == '>')
+                {
+                    if (i % 2 == 0)
+                    {
+                        x1++;
+                    }
+                    else
+                    {
+                        x2++;
+                    }
+                }
+                if (i % 2 == 0)
+                {
+                    if (houses.ContainsKey($"{x1};{y1}"))
+                    {
+                        houses[$"{x1};{y1}"]++;
+                    }
+                    else
+                    {
+                        houses.Add($"{x1};{y1}", 1);
+                        visit++;
+                    }
+                }
+                else
+                {
+                    if (houses.ContainsKey($"{x2};{y2}"))
+                    {
+                        houses[$"{x2};{y2}"]++;
+                    }
+                    else
+                    {
+                        houses.Add($"{x2};{y2}", 1);
+                        visit++;
+                    }
+                }
+                
+                
+            }
+            
+            Console.WriteLine(visit);
+        }
+    }
+}
---
+++ day04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace day04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string prae = "yzbqklnj";
+            int num = -1;
+            int result;
+            
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            
+            while (true)
+            {
+                num++;
+                string input = prae + num.ToString();
+                
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                if (hashBytes[0] == 0 && hashBytes[1] == 0 && hashBytes[2] == 0)
+                {
+                    result = num;
+                    Console.WriteLine(result);
+                    break;
+                }
+            }
+        }
+    }
+}
