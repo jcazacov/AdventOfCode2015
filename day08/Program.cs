1c6cf8f927105450b5bb2239deaf08c2d255f34d
Jacob Cazacov <jacob.cazacov@gmail.com>
2021-11-06 21:37:02 +0100

    day09

---
+++ day08/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace day08
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int literalchar = 0;
+            int memorychar = 0;
+            int expandchar = 0;
+            
+
+            List<string> lines = new List<string>();
+            string[] allLines = File.ReadAllLines("Input.txt");
+            foreach (var line in allLines)
+            {
+                lines.Add(line);
+            }
+            foreach (var line in lines)
+            {
+                literalchar += line.Length;
+            }
+
+            foreach (string line in lines)
+            {
+                for (int i = 1; i < line.Length - 1; i++)
+                {
+                    char curchar = line[i];
+                    char nextchar = line[i + 1];
+                    if (line[i] == '\\')
+                    {
+                        if (line[i + 1] == '\\')
+                        {
+                            memorychar += 1;
+                            i++;
+                        }
+
+                        else
+                        {
+                            if (line[i + 1] == '\"')
+                            {
+                                memorychar += 1;
+                                i++;
+                            }
+                            else
+                            {
+                                if (line[i + 1] == 'x')
+                                {
+                                    memorychar += 1;
+                                    i = i + 3;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        memorychar++;
+                    }
+                }
+            }
+
+            foreach (string line in lines)
+            {
+                expandchar += 2;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    char curchar = line[i];
+                    if (line[i] == '\\')
+                    {
+                        expandchar++;
+                    }
+                    if (line[i] == '\"')
+                    {
+                        expandchar++;
+                    }
+
+                    expandchar++;
+                }
+            }
+
+
+
+            Console.WriteLine($"There are {literalchar} literal characters on the list");
+            Console.WriteLine($"There are {memorychar} characters saved in memory");
+            Console.WriteLine($"There are {expandchar} needed to encode the whole list");
+            Console.WriteLine($"there are {literalchar - memorychar} less characters saved in memory than written on the list");
+            Console.WriteLine($"there are {expandchar - literalchar} more characters needed to encode the list then are written on the list.");
+        }
+    }
+}
---
+++ day09/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day09
+{
+    internal class Program
+    {
+        static Dictionary<string, int> distances = new Dictionary<string, int>();
+        
+        public static void Main(string[] args)
+        {
+
+            HashSet<string> allCities = new HashSet<string>();
+            string pattern = @"(\w+) to (\w+) = (\d+)";
+
+            string[] allLines = File.ReadAllLines("Input.txt");
+            foreach (var line in allLines)
+            {
+                var match = Regex.Match(line, pattern);
+                if (match.Success)
+                {
+                    string city1 = match.Groups[1].Value;
+                    string city2 = match.Groups[2].Value;
+                    int distance = Int32.Parse(match.Groups[3].Value);
+
+                    allCities.Add(city1);
+                    allCities.Add(city2);
+                    
+                    distances.Add(city1 + city2, distance);
+                    distances.Add(city2 + city1, distance);
+
+                }
+            }
+
+            List<string> cityList = allCities.ToList();
+
+            int result = minDistance(null, cityList);
+            Console.WriteLine(result);
+
+
+
+        }
+
+        static int distance(string city1, string city2)
+        {
+            if (city1 == null)
+            {
+                return 0;
+            }
+
+            string myKey = city1 + city2;
+            return distances[myKey];
+        }
+
+        static int minDistance(string start, List<string> citiesLeft)
+        {
+            if (citiesLeft.Count == 0)
+            {
+                return 0; 
+            }
+            int mindin = Int32.MaxValue;
+
+            foreach (string city in citiesLeft)
+            {
+                int newDistance = distance(start, city) + minDistance(city,
+                    citiesLeft.Where(c => c != city).ToList());
+                if (newDistance < mindin)
+                {
+                    mindin = newDistance;
+                }
+            }
+            return mindin;
+        }
+    }
+}
