4c2ffe204630d71c8aa1db94b0acf2582c48da1a
Jacob Cazacov <jacob.cazacov@gmail.com>
2021-11-21 12:24:57 +0100

    day12 part 1

---
+++ day12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace day12
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pattern = @"(-*)(\d+)";
+            int sum = 0;
+
+            string allLines = File.ReadAllText("Input.txt");
+            var regex = new Regex(pattern);
+            
+            
+            var match = regex.Match(allLines);
+            while (match.Success)
+            {
+                int zahl = Int32.Parse(match.Groups[2].Value);
+                if (match.Groups[1].Value == "-")
+                {
+                    sum = sum - zahl;
+                }
+                else
+                {
+                    sum = sum + zahl;
+                }
+                
+                match = match.NextMatch();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
